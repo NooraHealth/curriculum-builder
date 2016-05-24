1a620710cd692bc6e2aaa7f576704a62d8bb07c2
Yihang Ho <hoyihang5@gmail.com>
2016-05-24 22:50:42 +0800

    Work on first part of the curriculum builder

--- imports/ui/pages/build_curriculum_page.jsx
+++ imports/ui/pages/build_curriculum_page.jsx
@@ -7,9 +7,62 @@
  *****/
 import React from 'react';
 
+const conditions = ["Cardiac Surgery", "Diabetes", "Neonatology"];
+const languages  = ["Hindi", "English", "Kannada", "Tamil"];
+
 const BuildCurriculumPage = React.createClass({
+  getInitialState() {
+    return {
+      title: "",
+      condition: conditions[0],
+      language: languages[0]
+    };
+  },
+  renderHeader() {
+    return (
+      <div>
+        <div className="field">
+          <label>Title</label>
+          <input type="text" name="title" placeholder="New Title" value={ this.state.title } onChange={ this.onTitleChange } />
+        </div>
+
+        <div className="field">
+          <label>Condition</label>
+          <select className="ui fluid dropdown" value={ this.state.condition } onChange={ this.onConditionChange }>
+            { conditions.map(condition => <option key={ condition }>{ condition }</option>) }
+          </select>
+        </div>
+
+        <div className="field">
+          <label>Language</label>
+          <select className="ui fluid dropdown" value={ this.state.language } onChange={ this.onLanguageChange }>
+            { languages.map(language => <option key={ language }>{ language }</option>) }
+          </select>
+        </div>
+      </div>
+    );
+  },
   render() {
-    return <div>Hello World</div>;
+    return (
+      <form className="ui form">
+        { this.renderHeader() }
+      </form>
+    );
+  },
+  onLanguageChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  },
+  onConditionChange(event) {
+    this.setState({
+      condition: event.target.value
+    });
+  },
+  onLanguageChange(event) {
+    this.setState({
+      language: event.target.value
+    });
   }
 });
 
