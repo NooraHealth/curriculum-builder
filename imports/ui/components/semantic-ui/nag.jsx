098d73cacbf86d39a3a2fdd4509bd9b15157bcff
Yihang Ho <hoyihang5@gmail.com>
2016-05-29 22:56:35 +0800

    Add documentation for the Nag component

--- imports/ui/components/semantic-ui/nag.jsx
+++ imports/ui/components/semantic-ui/nag.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+// React wrapper on top of Semantic UI Nag.
+//
+// Props:
+// onDimiss - Callback when the user clicks on the cross. For the sake of good
+//            UX, this callback should always be provided.
+//
+// Note:
+// This component is completely pure - it will not dismiss itself, thankfully,
+// unlike the Modal component.
+
 const Nag = React.createClass({
   propTypes: {
     onDismiss: React.PropTypes.func.isRequired
