3e85e2e71fd83287928f004856187ce07ed23839
Yihang Ho <hoyihang5@gmail.com>
2016-06-19 15:03:58 +0800

    Preview lesson image

--- imports/ui/pages/build_curriculum_page/lessons_list_item.jsx
+++ imports/ui/pages/build_curriculum_page/lessons_list_item.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { imageURL } from '../../../uploads/image';
+
 const LessonsListItem = React.createClass({
   propTypes: {
     // Perhaps change this to an actual Lesson model or something?
@@ -14,10 +16,26 @@ const LessonsListItem = React.createClass({
     }
   },
   render() {
+    // TODO move these style to CSS
+
+    const containerStyle = {
+      display: 'flex'
+    };
+
+    const contentStyle = {
+      flexGrow: 1
+    };
+
     return (
-      <div className="item">
-        <div className="header">{ this.props.lesson.title }</div>
-        { this.renderEditButton() }
+      <div className="item" style={ containerStyle }>
+        <div className="ui small image">
+          <img src={ imageURL(this.props.lesson.image) } />
+        </div>
+
+        <div style={ contentStyle }>
+          <div className="header">{ this.props.lesson.title }</div>
+          { this.renderEditButton() }
+        </div>
       </div>
     );
   }
