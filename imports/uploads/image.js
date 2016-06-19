67a92773d61859a4b8da44d1c27aa18e58f768d1
Yihang Ho <hoyihang5@gmail.com>
2016-06-19 14:43:27 +0800

    Export function to determine full image URL

--- imports/uploads/image.js
+++ imports/uploads/image.js
@@ -3,6 +3,8 @@ import { Slingshot } from 'meteor/edgee:slingshot';
 import path from 'path';
 import uuid from 'node-uuid';
 
+const { S3Bucket: bucket, S3Region: region } = Meteor.settings.public;
+
 Slingshot.fileRestrictions("imageUploads", {
   allowedFileTypes: ["image/jpeg", "image/png"],
   maxSize: 10 * 1024 * 1024
@@ -10,8 +12,8 @@ Slingshot.fileRestrictions("imageUploads", {
 
 if (Meteor.isServer) {
   Slingshot.createDirective("imageUploads", Slingshot.S3Storage, {
-    bucket: Meteor.settings.S3Bucket,
-    region: Meteor.settings.S3Region,
+    bucket,
+    region,
     acl: "public-read",
     authorize() {
       return true;
@@ -23,4 +25,8 @@ if (Meteor.isServer) {
       return `images/${filename}${extension}`;
     }
   });
+}
+
+export function imageURL(filename) {
+  return `https://${bucket}.s3-${region}.amazonaws.com/images/${filename}`;
 }
