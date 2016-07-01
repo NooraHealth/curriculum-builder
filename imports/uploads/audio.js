1ab05fc7c9726e1fa862c16b998d79216ccfcac8
Yihang Ho <hoyihang5@gmail.com>
2016-07-01 09:38:21 +0800

    Implement audioUploads and videoUploads
    Currently audioUploads, imageUploads, and videoUploads are very
    repetitive. Perhaps we can dry them up someday.

---
+++ imports/uploads/audio.js
@@ -0,0 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import { Slingshot } from 'meteor/edgee:slingshot';
+import path from 'path';
+import uuid from 'node-uuid';
+
+const { S3Bucket: bucket, S3Region: region } = Meteor.settings.public;
+
+Slingshot.fileRestrictions("audioUploads", {
+  allowedFileTypes: ["audio/m4a", "audio/x-m4a", "audio/mp3", "audio/wav", "audio/x-wav", "audio/ogg", "audio/aac"],
+  maxSize: 10 * 1024 * 1024
+});
+
+if (Meteor.isServer) {
+  Slingshot.createDirective("audioUploads", Slingshot.S3Storage, {
+    bucket,
+    region,
+    acl: "public-read",
+    authorize() {
+      return true;
+    },
+    key(file) {
+      const extension = path.extname(file.name);
+      const filename  = uuid.v4().replace(/\-/g, "");
+
+      return `NooraHealthContent/Audio/${filename}${extension}`;
+    }
+  });
+}
+
+export function audioURL(filename) {
+  return `https://${bucket}.s3-${region}.amazonaws.com/NooraHealthContent/Audio/${filename}`;
+}
---
+++ imports/uploads/video.js
@@ -0,0 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import { Slingshot } from 'meteor/edgee:slingshot';
+import path from 'path';
+import uuid from 'node-uuid';
+
+const { S3Bucket: bucket, S3Region: region } = Meteor.settings.public;
+
+Slingshot.fileRestrictions("videoUploads", {
+  allowedFileTypes: ["video/mp4", "video/quicktime"],
+  maxSize: 10 * 1024 * 1024
+});
+
+if (Meteor.isServer) {
+  Slingshot.createDirective("videoUploads", Slingshot.S3Storage, {
+    bucket,
+    region,
+    acl: "public-read",
+    authorize() {
+      return true;
+    },
+    key(file) {
+      const extension = path.extname(file.name);
+      const filename  = uuid.v4().replace(/\-/g, "");
+
+      return `NooraHealthContent/Video/${filename}${extension}`;
+    }
+  });
+}
+
+export function audioURL(filename) {
+  return `https://${bucket}.s3-${region}.amazonaws.com/NooraHealthContent/Video/${filename}`;
+}
