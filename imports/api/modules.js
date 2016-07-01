be05d4e67bf763556888dff1706d2a0ed02401e9
Yihang Ho <hoyihang5@gmail.com>
2016-07-01 17:32:25 +0800

    Implement API for modules

---
+++ imports/api/modules.js
@@ -0,0 +1,36 @@
+import { Meteor } from 'meteor/meteor';
+
+import Immutable from 'immutable';
+
+import { Modules } from 'meteor/noorahealth:mongo-schemas';
+
+const Module = Immutable.Record({
+  _id: '',
+  type: "MULTIPLE_CHOICE",
+  title: undefined,
+  image: undefined,
+  question: undefined,
+  options: undefined,
+  correct_answer: undefined,
+  correct_audio: undefined,
+  video: undefined,
+  audio: undefined
+});
+
+export { Module };
+
+Meteor.methods({
+  'modules.upsert'(module) {
+    return Modules.upsert(module._id, {'$set': {
+      type: module.type,
+      title: module.title,
+      image: module.image,
+      question: module.question,
+      options: module.options,
+      correct_answer: module.correct_answer,
+      correct_audio: module.correct_audio,
+      video: module.video,
+      audio: module.audio
+    }});
+  }
+});
