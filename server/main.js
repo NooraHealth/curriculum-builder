60f5e595a6a2aa7d7b929813122cf1a9157d81c7
Yihang Ho <hoyihang5@gmail.com>
2016-07-01 17:32:46 +0800

    Import necessary stuff on the server

--- server/main.js
+++ server/main.js
@@ -2,7 +2,11 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/curriculums';
 import '../imports/api/lessons';
+import '../imports/api/modules';
+
+import '../imports/uploads/audio';
 import '../imports/uploads/image';
+import '../imports/uploads/video';
 
 Meteor.startup(() => {
   // code to run on server at startup
